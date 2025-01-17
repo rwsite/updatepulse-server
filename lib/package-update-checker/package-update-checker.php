0e0d64426433c50e9b40891895a20666e6e96a29
Alexandre Froger <3622456+froger-me@users.noreply.github.com>
2025-01-17 22:35:23 +0800

    Software licenses

--- lib/package-update-checker/package-update-checker.php
+++ lib/package-update-checker/package-update-checker.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Package Update Checker Library v1
+ * Package Update Checker Library for UpdatePulse Server
  * https://anyape.com
  *
  * Copyright 2025 Anyape
+ * Forked and heavily trimmed-down version of Plugin Update Checker Library (5.3)
  * Released under the MIT license. See license.txt for details.
  */
 
