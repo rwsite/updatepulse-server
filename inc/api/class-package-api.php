8ad30b17df7b16b9a194b2c0959241bbea3edbab
Alexandre Froger <3622456+froger-me@users.noreply.github.com>
2025-01-27 11:50:59 +0800

    typo

--- inc/api/class-package-api.php
+++ inc/api/class-package-api.php
@@ -372,7 +372,7 @@ class Package_API {
 	public function upserv_api_package_actions( $actions ) {
 		$actions['browse']     = __( 'Get information about multiple packages', 'updatepulse-server' );
 		$actions['read']       = __( 'Get information about a single package', 'updatepulse-server' );
-		$actions['edit']       = __( 'Forcefully download and overwrite an existing package on the file system.; requires using a VCS', 'updatepulse-server' );
+		$actions['edit']       = __( 'Forcefully download and overwrite an existing package on the file system; requires using a VCS', 'updatepulse-server' );
 		$actions['add']        = __( 'Download a package to the file system if it does not exist; requires using a VCS', 'updatepulse-server' );
 		$actions['delete']     = __( 'Delete a package from the file system', 'updatepulse-server' );
 		$actions['signed_url'] = __( 'Retrieve secure URLs for downloading packages', 'updatepulse-server' );
